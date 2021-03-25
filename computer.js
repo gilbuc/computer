ec1291806607acf6d1b331f2771f15cc33275acf
Gilbert Buchner <gibert@familie-buchner.at>
2021-03-25 15:39:37 +0100

    initial commit

---
+++ computer.js
@@ -0,0 +1,41 @@
+data.forEach(toolbox => {
+    let tb = document.createElement("div");
+    tb.setAttribute("class", "toolbox");
+    let tb_title = document.createElement("div");
+    tb_title.setAttribute("class", "title");
+    tb_title.appendChild(document.createTextNode(toolbox.title));
+    tb.appendChild(tb_title);
+    console.log(toolbox.toolblocks);
+
+    toolbox.toolblocks.forEach(toolblock => {
+        // Toolblock-Title
+        let tblk = document.createElement("div");
+        tblk.setAttribute("class", "toolblock");
+
+        let tblk_title = document.createElement("div");
+        tblk_title.setAttribute("class", "toolblock-title");
+        tblk_title.appendChild(document.createTextNode(toolblock.title));
+        tblk.appendChild(tblk_title);
+        // Toolblock-Entries
+        let ul = document.createElement("ul");
+        ul.setAttribute("class", "toolblock")
+        let colorNr = 1;
+        toolblock.entries.forEach(entry => {
+            let li = document.createElement("li");
+            li.setAttribute("class", "color" + colorNr);
+            colorNr < 4 ? colorNr++ : colorNr = 1;
+            let a = document.createElement("a");
+            let text = document.createTextNode(entry.title)
+            a.appendChild(text);
+            a.setAttribute("href", entry.url);
+            a.setAttribute("target", "_blank");
+            console.log(li);
+            li.appendChild(a);
+            ul.appendChild(li)
+        });
+        tblk.appendChild(ul);
+        tb.appendChild(tblk);
+    });
+
+    document.getElementById("app").appendChild(tb);
+});
---
+++ data.js
@@ -0,0 +1,37 @@
+let data = [{
+        title: "home",
+        toolblocks: [{
+                title: "buchnas",
+                entries: [
+                    { url: "https://buch-nas:5001/", title: "Admin" },
+                    { url: "http://buch-nas:30180/", title: "Gitea" },
+                    { url: "http://buch-nas/photo", title: "Photo" },
+                    { url: "http://buch-nas/audio", title: "Audio" }
+                ]
+            },
+            {
+                title: "world4you",
+                entries: [
+                    { url: "https://buch-nas:5001/", title: "Admin" },
+                    { url: "http://buch-nas:30180/", title: "Gitea" },
+                    { url: "http://buch-nas/photo", title: "Photo" },
+                    { url: "http://buch-nas/audio", title: "Audio" }
+                ]
+            }
+        ]
+    },
+
+    {
+        title: "development",
+        toolblocks: [{
+            title: "javascript",
+            entries: [
+                { url: "https://exploringjs.com/impatient-js/toc.html", title: "JavaScript for impatient programmers" },
+                { url: "https://reactjs.org/docs", title: "React" },
+
+            ]
+        }]
+    }
+
+
+];
